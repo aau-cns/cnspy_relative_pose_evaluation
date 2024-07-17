9a1bfcd8f6e010e5d5524cf12e81b2e86aeaa288
rojung <roland.jung@aau.at>
2024-07-18 00:42:23 +0200

    [ADD] ROSBag_TrueRelPoses():

--- cnspy_relative_pose_evaluation/ROSBag_TrueRelPoses.py
+++ cnspy_relative_pose_evaluation/ROSBag_TrueRelPoses.py
@@ -35,16 +35,56 @@ from cnspy_ranging_evaluation.ROSBag_TrueRanges import HistoryBuffer, get_key_fr
 from std_msgs.msg import Header, Time
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, TransformStamped
 
+def interpolate_pose(pose_hist, timestamp):
+    T_GLOBAL_BODY_Ti = pose_hist.get_at_t(timestamp)
+    if T_GLOBAL_BODY_Ti is None:
+        [t1, T_GLOBAL_BODY_T1] = pose_hist.get_before_t(timestamp)
+        [t2, T_GLOBAL_BODY_T2] = pose_hist.get_after_t(timestamp)
 
-class ROSBag_Poses2RelPose:
+        if t1 is None or t2 is None:
+            # if verbose:
+            #    print("* skip measurement from topic=" + topic + " at t=" + str(timestamp))
+            return None
+        dt = t2 - t1
+        dt_i = timestamp - t1
+        i = abs(dt_i / dt)
+
+        # interpolate between poses:
+        q0 = UnitQuaternion(T_GLOBAL_BODY_T1.R)
+        q1 = UnitQuaternion(T_GLOBAL_BODY_T2.R)
+        p0 = T_GLOBAL_BODY_T1.t
+        p1 = T_GLOBAL_BODY_T2.t
+
+        qr = UnitQuaternion(qslerp(q0.vec, q1.vec, i, shortest=True), norm=True)
+        pr = p0 * (1 - i) + i * p1
+
+        T_GLOBAL_BODY_Ti = SE3.Rt(qr.R, pr, check=True)
+        if not SE3.isvalid(T_GLOBAL_BODY_Ti, check=True):
+            if T_GLOBAL_BODY_Ti.A is None:
+                return None
+            else:
+                q = UnitQuaternion(SO3(T_GLOBAL_BODY_Ti.R, check=False), norm=True).unit()
+                T_GLOBAL_BODY_Ti = SE3.Rt(q.R, T_GLOBAL_BODY_Ti.t, check=True)
+    return T_GLOBAL_BODY_Ti
+
+
+class ROSBag_TrueRelPoses:
     def __init__(self):
         pass
 
     @staticmethod
-    def extract(bagfile_name, cfg, filename, result_dir="", verbose=False):
+    def extract(bagfile_in_name,
+                bagfile_out_name,
+                cfg,
+                stddev_pos=0.0,
+                perc_outliers=0.0,
+                stddev_outlier=0.5,
+                use_header_timestamp=False,
+                verbose=False
+                ):
 
-        if not os.path.isfile(bagfile_name):
-            print("ROSBag_Poses2RelPose: could not find file: %s" % bagfile_name)
+        if not os.path.isfile(bagfile_in_name):
+            print("ROSBag_Poses2RelPose: could not find file: %s" % bagfile_in_name)
             return False
         cfg = os.path.abspath(cfg)
         if not os.path.isfile(cfg):
@@ -53,18 +93,33 @@ class ROSBag_Poses2RelPose:
 
         if verbose:
             print("ROSBag_Poses2RelPose:")
-            print("* bagfile name: " + str(bagfile_name))
-            print("* cfg: \t " + str(cfg))
-            print("* filename: " + str(filename))
+            print("* bagfile in name: " + str(bagfile_in_name))
+            print("* bagfile out name: " + str(bagfile_out_name))
+            print("* cfg YAML file: \t " + str(cfg))
+            print("* stddev_pos: " + str(stddev_pos))
+            print("* perc_outliers: " + str(perc_outliers))
+            print("* outlier_stddev: " + str(stddev_outlier))
+            print("* use_header_timestamp: " + str(use_header_timestamp))
 
         ## Open BAG file:
         try:
-            bag = rosbag.Bag(bagfile_name)
+            bag = rosbag.Bag(bagfile_in_name)
         except:
             if verbose:
                 print("ROSBag_Poses2RelPose: Unexpected error!")
 
             return False
+
+        ## create result dir:
+        [root, ext] = os.path.splitext(bagfile_out_name)
+        [head, tail] = os.path.split(root)
+        try:  # else already exists
+            os.makedirs(head)
+        except:
+            pass
+
+        if verbose:
+            print("* result_dir: \t " + str(head))
 
         dict_cfg = None
         with open(cfg, "r") as yamlfile:
@@ -77,12 +132,16 @@ class ROSBag_Poses2RelPose:
             if "sensor_topics" not in dict_cfg:
                 print("[sensor_topics] does not exist in fn=" + cfg)
                 return False
+            if "relpose_topics" not in dict_cfg:
+                print("[relpose_topics] does not exist in fn=" + cfg)
+                return False
             print("Read successful")
         if verbose:
             print("configuration contains:")
             print("sensor_positions:" + str(dict_cfg["sensor_positions"]))
             print("sensor_orientations:" + str(dict_cfg["sensor_orientations"]))
             print("sensor_topics:" + str(dict_cfg["sensor_topics"]))
+            print("relpose_topics:" + str(dict_cfg["relpose_topics"]))
 
         info_dict = yaml.load(bag._get_yaml_info(), Loader=yaml.FullLoader)
 
@@ -92,65 +151,31 @@ class ROSBag_Poses2RelPose:
             bag.close()
             return False
 
-        ## create result dir:
-        if result_dir == "":
-            folder = str(bagfile_name).replace(".bag", "")
-        else:
-            folder = result_dir
-
-        folder = os.path.abspath(folder)
-        try:  # else already exists
-            os.makedirs(folder)
-        except:
-            pass
-
-        if verbose:
-            print("* result_dir: \t " + str(folder))
-
         ## create csv file according to the topic names:
         dict_file_writers = dict()
         dict_header_written = dict()
         dict_csvfile_hdls = dict()
         idx = 0
 
-        ## create a file writer for each sensor topic
-        for  id, topicName in dict_cfg["sensor_topics"].items():
-            if filename is None:
-                fn = str(folder + '/') + str.replace(topicName[1:], '/', '_') + '_sensor_' + str(id) + '.csv'
-            else:
-                fn = str(folder + '/') + str(filename) + '_' + str(id) + '.csv'
-
-            csvfile = open(fn, 'w+')
-            file_writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
-            dict_file_writers[topicName] = file_writer
-            dict_header_written[topicName] = False
-            dict_csvfile_hdls[topicName] = csvfile
-
-            if verbose:
-                print("ROSbag_Pose2Ranges: creating csv file: %s " % fn)
-
         ## check if desired topics are in the bag file:
         num_messages = info_dict['messages']
         bag_topics = info_dict['topics']
 
-        found = True
+        # check if desired topics are in
         for id, topicName in dict_cfg["sensor_topics"].items():
             found_ = False
             for topic_info in bag_topics:
                 if topic_info['topic'] == topicName:
                     found_ = True
-                    pass
-                pass
             if not found_:
-                print("# ERROR: desired topic [" + str(topicName) + "] is not in bag file!")
-
-            found = found & found_
-            pass
-        if not found:
-            print("\nROSBag_Poses2RelPose: not all topics found! Stopping here...")
-            for topicName in dict_cfg["sensor_topics"]:
-                print(" * " + topicName)
-            return False
+                print("# WARNING: desired topic [" + str(topicName) + "] is not in bag file!")
+        for id, topicName in dict_cfg["relpose_topics"].items():
+            found_ = False
+            for topic_info in bag_topics:
+                if topic_info['topic'] == topicName:
+                    found_ = True
+            if not found_:
+                print("# WARNING: desired topic [" + str(topicName) + "] is not in bag file!")
 
         if verbose:
             print("\nROSBag_Poses2RelPose: num messages " + str(num_messages))
@@ -221,75 +246,61 @@ class ROSBag_Poses2RelPose:
             dict_history[topic] = HistoryBuffer(dict_t=poses)
 
         ## TODO:
+        cnt = 0;
         try:  # else already exists
-            for topic, hist in dict_history.items():
-                if verbose:
-                    print("\nROSBag_Poses2RelPose: compute relative pose with respect to topic=[" + topic + "]...")
-                for idx in tqdm(range(0, len(hist.t_vec)), total= len(hist.t_vec), unit="poses"):
-                    file_writer = dict_file_writers[topic]
-                    ID1 = get_key_from_value(dict_cfg["sensor_topics"], topic)
-                    T_GLOBAL_SENSOR1 = hist.val_vec[idx]
-                    timestamp = hist.t_vec[idx]
-                    for ID2, topic2 in dict_cfg["sensor_topics"].items():
-                        if topic != topic2:
-                            #ID2 = get_key_from_value(dict_cfg["sensor_topics"], topic2)
+            print("ROSbag_TrueRanges: computing new relative pose measurements...")
+            with rosbag.Bag(bagfile_out_name, 'w') as outbag:
+                for topic, msg, t in tqdm(bag.read_messages(), total=num_messages, unit="msgs"):
+                    if topic in dict_cfg["relpose_topics"].values() and hasattr(msg, 'poses') and hasattr(msg, 'header'):
+                        ID1 = get_key_from_value(dict_cfg["relpose_topics"], topic)
+                        idx_pose = 0
+                        for relpose in msg.poses: # id, pose, covariance
+                            ID2 = relpose.id
+                            timestamp = round(msg.header.stamp.to_sec(), round_decimals)
+                            for id2, topic2 in dict_cfg["sensor_topics"].items():
+                                if id2 == relpose.id:
+                                    #ID2 = get_key_from_value(dict_cfg["sensor_topics"], topic2)
 
-                            T_GLOBAL_SENSOR2 = None
-                            if  dict_history[topic2].exists_at_t(timestamp) is None:
-                                [t1, T_GLOBAL_BODY_T1] = dict_history[topic2].get_before_t(timestamp)
-                                [t2, T_GLOBAL_BODY_T2] = dict_history[topic2].get_after_t(timestamp)
+                                    T_GLOBAL_SENSOR1 = interpolate_pose(dict_history[dict_cfg["sensor_topics"][ID1]], timestamp)
+                                    T_GLOBAL_SENSOR2 = interpolate_pose(dict_history[dict_cfg["sensor_topics"][ID2]], timestamp)
 
-                                if t1 is None or t2 is None:
-                                    #if verbose:
-                                    #    print("* skip measurement from topic=" + topic + " at t=" + str(timestamp))
-                                    continue
-                                dt = t2 - t1
-                                dt_i = timestamp - t1
-                                i = abs(dt_i / dt)
+                                    if T_GLOBAL_SENSOR1 is not None and T_GLOBAL_SENSOR2 is not None:
+                                        T_SENSOR1_SENSOR2 = SE3(T_GLOBAL_SENSOR1 * T_GLOBAL_SENSOR2.inv())
+                                        p = T_SENSOR1_SENSOR2.t
+                                        q = UnitQuaternion(T_SENSOR1_SENSOR2.R)
+                                        qv = q.vec
 
-                                # interpolate between poses:
-                                q0 = UnitQuaternion(T_GLOBAL_BODY_T1.R)
-                                q1 = UnitQuaternion(T_GLOBAL_BODY_T2.R)
-                                p0 = T_GLOBAL_BODY_T1.t
-                                p1 = T_GLOBAL_BODY_T2.t
+                                        # TODO: add noise etc.
 
-                                qr = UnitQuaternion(qslerp(q0.vec, q1.vec, i, shortest=True), norm=True)
-                                pr = p0 * (1 - i) + i * p1
-
-
-                                T_GLOBAL_SENSOR2 = SE3.Rt(qr.R, pr, check=True)*dict_T_BODY_SENSOR[topic2]
-                                if not SE3.isvalid(T_GLOBAL_SENSOR2, check=True):
-                                    if T_GLOBAL_SENSOR2.A is None:
-                                        if verbose:
-                                            print(
-                                                "* interp failed: skip measurement from topic=" + topic + " at t=" + str(
-                                                    timestamp))
-                                        continue
+                                        msg.poses[idx_pose].pose.position.x = p[0]
+                                        msg.poses[idx_pose].pose.position.y = p[1]
+                                        msg.poses[idx_pose].pose.position.z = p[2]
+                                        msg.poses[idx_pose].pose.orientation.x = qv[0]
+                                        msg.poses[idx_pose].pose.orientation.y = qv[1]
+                                        msg.poses[idx_pose].pose.orientation.z = qv[2]
+                                        msg.poses[idx_pose].pose.orientation.w = qv[3]
+                                        if use_header_timestamp and hasattr(msg, "header"):
+                                            outbag.write(topic, msg, msg.header.stamp)
+                                        else:
+                                            outbag.write(topic, msg, t)
+                                        cnt += 1
+                                        pass
                                     else:
-                                        q = UnitQuaternion(SO3(T_GLOBAL_SENSOR2.R, check=False), norm=True).unit()
-                                        T_GLOBAL_SENSOR2 = SE3.Rt(q.R, T_GLOBAL_SENSOR2.t, check=True)
-                                pass
-                            else:
-                                T_GLOBAL_SENSOR2 = dict_history[topic2].get_at_t(timestamp)*dict_T_BODY_SENSOR[topic2]
+                                        continue
 
-                            T_SENSOR1_SENSOR2 = SE3(T_GLOBAL_SENSOR1 * T_GLOBAL_SENSOR2.inv())
-                            t = T_SENSOR1_SENSOR2.t
-                            q = UnitQuaternion(T_SENSOR1_SENSOR2.R)
-                            qv = q.vec
-                            if not dict_header_written[topic]:
-                                file_writer.writerow(["t", "ID1", "ID2", "tx", "ty", "tz", "qw", "qx", "qy", "qz"])
-                                dict_header_written[topic] = True
+                            idx_pose += 1
 
-                            content = ["%f" % timestamp, str(ID1), str(ID2),
-                                       str(round(t[0], round_decimals)),
-                                       str(round(t[1], round_decimals)),
-                                       str(round(t[2], round_decimals)),
-                                       str(round(qv[3], round_decimals)),
-                                       str(round(qv[0], round_decimals)),
-                                       str(round(qv[1], round_decimals)),
-                                       str(round(qv[2], round_decimals))]
+                    else:
+                        # write other topic
+                        if use_header_timestamp and hasattr(msg, "header"):
+                            outbag.write(topic, msg, msg.header.stamp)
+                        else:
+                            outbag.write(topic, msg, t)
+                        pass
 
-                            file_writer.writerow(content)
+
+
+
         except  Exception as e:
             print("ROSBag_Poses2RelPose: Unexpected error while creating the CSV files! msg=%s" % repr(e))
             print(str(sys.exc_info()))
@@ -303,7 +314,7 @@ class ROSBag_Poses2RelPose:
         for topic, header_writen in dict_header_written.items():
             if not header_writen:
                 print("\nROSBag_Poses2RelPose: \n\tWARNING topic [" + str(topic) + "] was not in bag-file")
-                print("\tbag file [" + str(bagfile_name) + "] contains: ")
+                print("\tbag file [" + str(bagfile_in_name) + "] contains: ")
                 # print(info_dict['topics'])
                 for t in info_dict['topics']:
                     print(t['topic'])
@@ -319,21 +330,35 @@ class ROSBag_Poses2RelPose:
 def main():
     # example: ROSBag_Pose2Ranges.py --bagfile ../test/sample_data//uwb_calib_a01_2023-08-31-21-05-46.bag --topic /d01/mavros/vision_pose/pose --cfg ../test/sample_data/config.yaml --verbose
     parser = argparse.ArgumentParser(
-        description='ROSBag_Poses2RelPose: extract given pose topics and compute for each pose a relative poses, which is stored into a CSV file')
-    parser.add_argument('--bagfile', help='input bag file', required=True)
-    parser.add_argument('--cfg', help='YAML configuration file describing the setup: {sensor_positions, sensor_topics}', default="config.yaml", required=True)
-    parser.add_argument('--filename', help='csv filename of corresponding topic', default="")
-    parser.add_argument('--result_dir', help='directory to store results [otherwise bagfile name will be a directory]',
-                        default='')
+        description='ROSBag_Poses2RelPose: extract given pose topics and compute for each relative poses measurement a true pose, which is stored into a CSV file')
+    parser.add_argument('--bagfile_in', help='input bag file', required=True)
+    parser.add_argument('--bagfile_out', help='output bag file', default="")
+    parser.add_argument('--cfg',
+                        help='YAML configuration file describing the setup: {rel_tag_positions, abs_anchor_positions}',
+                        default="config.yaml", required=True)
     parser.add_argument('--verbose', action='store_true', default=False)
+    parser.add_argument('--std_pos',
+                        help='standard deviation of generated measurements: z = d + white_noise(std_range)',
+                        default=0.1)
+    parser.add_argument('--perc_outliers', help='specifies a percentage of generated outliers by modified the '
+                                                'measurement: z = d + white_noise(std_range) + std_range',
+                        default=0.0)
+    parser.add_argument('--outlier_stddev', help='standard deviation of the outliers.',
+                        default=1.0)
+    parser.add_argument('--use_header_timestamp', action='store_true',
+                        help='overwrites the bag time with the header time stamp', default=False)
 
     tp_start = time.time()
     args = parser.parse_args()
 
-    if ROSBag_Poses2RelPose.extract(bagfile_name=args.bagfile, topic=str(args.topic), cfg=args.cfg,
-                          filename=args.filename, result_dir=args.result_dir,
-                          verbose=args.verbose
-                          ):
+    if ROSBag_TrueRelPoses.extract(bagfile_in_name=args.bagfile_in,
+                                 bagfile_out_name=args.bagfile_out,
+                                 cfg=args.cfg,
+                                 verbose=args.verbose,
+                                 stddev_pos=float(args.std_pos),
+                                 perc_outliers=float(args.perc_outliers),
+                                 stddev_outlier=float(args.outlier_stddev),
+                                 use_header_timestamp=args.use_header_timestamp ):
         print(" ")
         print("finished after [%s sec]\n" % str(time.time() - tp_start))
     else:
